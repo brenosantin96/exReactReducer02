32cc84c3a0aae2a2943f64ce7e11503d762acc22
Breno Santin <brenolimaa96@gmail.com>
2022-04-16 06:25:07 -0300

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fundamentos Reducer React 02
+
+## Exemplo real de como usar um reducer
+
+No projeto em específico é criado reducer onde alteramos um array de pessoas. 
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,62 @@
-import { useState } from 'react'
+import { usePeopleList } from './reducers/peopleList';
+import { ChangeEvent, useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
+  const [list, dispatch] = usePeopleList();
+  const [nameInput, setNameInput] = useState('');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNameInput(e.target.value);
+  }
+
+  const handleAddButton = () => {
+    if (nameInput) {
+      dispatch({
+        type: 'ADD',
+        payload: {
+          name: nameInput
+        }
+      });
+
+      setNameInput('');
+    }
+  };
+
+  const deletePerson = (id: string) => {
+    dispatch(
+      {
+        type: 'DEL',
+        payload: { id }
+      });
+  }
+
+  const handleOrderButton = () => {
+    dispatch(
+      {
+        type: 'ORDER'
+      }
+    );
+  }
 
   return (
     <div>
-      
+
+      <input type="text" value={nameInput} onChange={handleInputChange} />
+      <button onClick={handleAddButton}>Adiciona</button>
+
+      <hr />
+
+      <button onClick={handleOrderButton}>Ordenar</button>
+
+      <h3>Lista de pessoas:</h3>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index} >{item.name} <button onClick={() => deletePerson(item.id)}>Deletar</button></li>
+
+        ))}
+      </ul>
     </div>
   )
 }
